2d3c677fb17a49b42c3b6f0f8a7bae4743eb62ed
UsamaWaqas <usamawaqas.m@gmail.com>
2021-05-27 12:21:19 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
 
 <div className = "App"> 
 <Dinner sweetdish = "kheer"  dish= "chicken baryani"/>
+<Dinner sweetdish = "kheer"  dish= "chicken baryani"/>
+<Dinner sweetdish = "kheer"  dish= "chicken baryani"/>
+<Dinner sweetdish = "kheer"  dish= "chicken baryani"/>
 
 </div>
  
